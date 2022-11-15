7a7ae3f06b5d1c85196f255f4ea8ce3c41c000e1
Oxozon <oxozon.o4@gmail.com>
2022-11-15 15:00:25 +0100

    finish calculator operations

--- app/src/main/java/com/oxozon/mycalculator/MainActivity.kt
+++ app/src/main/java/com/oxozon/mycalculator/MainActivity.kt
@@ -61,20 +61,65 @@ class MainActivity : AppCompatActivity() {
                     var prefix = "-"
                     tvValue = tvValue.substring(1)
                 }
-                val splitValue = tvValue.split("-")
+                if (tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
 
-                var one = splitValue[0]
-                var two = splitValue[1]
+                    var one = splitValue[0]
+                    var two = splitValue[1]
 
-                if (prefix.isNotEmpty()) {
-                    one = prefix + one
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                } else if (tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                } else if (tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                } else if (tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
                 }
 
-                tvInput?.text = (one.toDouble() - two.toDouble()).toString()
             } catch (e: java.lang.ArithmeticException) {
                 e.printStackTrace()
             }
         }
+    }
+
+
+    private fun removeZeroAfterDot(result: String): String {
+        var value = result
+        if (value.contains(".0")) {
+            value = result.substring(0, result.length - 2)
+        }
+        return value
     }
 
     private fun isOperatorAdded(value: String): Boolean {

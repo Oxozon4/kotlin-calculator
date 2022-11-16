4813b95c65e748d4d95f760281bca0bed68a6ad6
Oxozon <oxozon.o4@gmail.com>
2022-11-16 04:33:42 +0100

    add 4 new activities

--- app/src/main/java/com/oxozon/mycalculator/MainActivity.kt
+++ app/src/main/java/com/oxozon/mycalculator/MainActivity.kt
@@ -2,176 +2,31 @@ package com.oxozon.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.content.Intent
+import android.view.View
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private var tvInput: TextView? = null
-    private var isLastCharNumeric: Boolean = false
-    private var isLastCharDot: Boolean = false
-    private var isLastCharBackspace: Boolean = false
+    var basicCalculatorButton: Button? = null
+    var advancedCalculatorButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tvInput = findViewById(R.id.tvInput)
-    }
 
-    fun onDigit(view: View) {
-        tvInput?.append((view as Button).text)
-        isLastCharNumeric = true
-        isLastCharDot = false
-        isLastCharBackspace = false
-    }
+        basicCalculatorButton = findViewById<Button>(R.id.btnBasicCalculator)
+        basicCalculatorButton?.setOnClickListener {
+            val intent = Intent(this, BasicCalculator::class.java)
+            startActivity(intent)
+        }
 
-    fun onClear(view: View) {
-        tvInput?.text = ""
-    }
-
-    fun onBackspace(view: View) {
-        if (isLastCharBackspace) {
-            onClear(view)
-            isLastCharBackspace = false
-        } else {
-            tvInput?.text = tvInput?.text?.dropLast(1)
-            isLastCharBackspace = true
-            val lastChar = tvInput?.text?.toString()?.last() ?: return
-            if (isValueOperator(lastChar)) {
-                isLastCharNumeric = false
-                isLastCharDot = false
-            } else if (lastChar.equals(".")) {
-                isLastCharNumeric = false
-                isLastCharDot = true
-            } else {
-                isLastCharNumeric = true
-                isLastCharDot = false
-            }
-
+        advancedCalculatorButton = findViewById<Button>(R.id.btnAdvancedCalculator)
+        advancedCalculatorButton?.setOnClickListener {
+            val intent = Intent(this, AdvancedCalculator::class.java)
+            startActivity(intent)
         }
     }
 
-    fun onDecimalPoint(view: View) {
-        if (isLastCharNumeric && !isLastCharDot) {
-            tvInput?.append(".")
-            isLastCharNumeric = false
-            isLastCharDot = true
-        }
-    }
-
-    fun onOperator(view: View) {
-        tvInput?.text?.let {
-            if (isLastCharNumeric && !isOperatorAdded(it.toString())) {
-                tvInput?.append((view as Button).text)
-                isLastCharDot = false
-                isLastCharNumeric = false
-                isLastCharBackspace = false
-            } else if (isLastCharNumeric && isOperatorAdded(it.toString())) {
-                onEqual(view)
-                tvInput?.append((view as Button).text)
-                isLastCharDot = false
-                isLastCharNumeric = false
-                isLastCharBackspace = false
-            }
-        }
-
-    }
-
-    fun onEqual(view: View) {
-        if (isLastCharNumeric) {
-            var tvValue = tvInput?.text.toString()
-            var prefix = ""
-            isLastCharBackspace = false
-
-            try {
-                if (tvValue.startsWith("-")) {
-                    prefix = "-"
-                    tvValue = tvValue.substring(1)
-                }
-                if (tvValue.contains("-")) {
-                    val splitValue = tvValue.split("-")
-
-                    var one = splitValue[0]
-                    var two = splitValue[1]
-
-                    if (prefix.isNotEmpty()) {
-                        one = prefix + one
-                    }
-
-                    tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
-                } else if (tvValue.contains("+")) {
-                    val splitValue = tvValue.split("+")
-
-                    var one = splitValue[0]
-                    var two = splitValue[1]
-
-                    if (prefix.isNotEmpty()) {
-                        one = prefix + one
-                    }
-
-                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
-                } else if (tvValue.contains("/")) {
-                    val splitValue = tvValue.split("/")
-
-                    var one = splitValue[0]
-                    var two = splitValue[1]
-
-                    if (prefix.isNotEmpty()) {
-                        one = prefix + one
-                    }
-
-                    tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
-                } else if (tvValue.contains("*")) {
-                    val splitValue = tvValue.split("*")
-
-                    var one = splitValue[0]
-                    var two = splitValue[1]
-
-                    if (prefix.isNotEmpty()) {
-                        one = prefix + one
-                    }
-
-                    tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
-                }
-
-            } catch (e: java.lang.ArithmeticException) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-    private fun onChangeSign(view: View) {
-
-    }
-
-    private fun removeZeroAfterDot(result: String): String {
-        var value = result
-        if (value.contains(".0")) {
-            value = result.substring(0, result.length - 2)
-        }
-        return value
-    }
-
-    private fun isOperatorAdded(value: String): Boolean {
-        return if (value.startsWith("-")) {
-            false
-        } else {
-            value.contains("/")
-                    || value.contains("*")
-                    || value.contains("+")
-                    || value.contains("-")
-        }
-    }
-
-    private fun isValueOperator(value: Char?): Boolean {
-        if (value == null) {
-            return false
-        }
-        return  value.equals("/")
-                || value.equals("*")
-                || value.equals("+")
-                || value.equals("-")
-    }
 }
